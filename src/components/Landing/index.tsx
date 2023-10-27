47e9b4343751767b2950767d72eaa71543b024ca
jena adkins <jena.adkins@creditone.com>
2023-10-27 07:34:19 -0700

    landing page text

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,9 +1,9 @@
-import { Container, Center, Flex } from "@prismane/core";
+import { Container, Box, Flex, Image } from "@prismane/core";
 import "./index.css";
 
 export const Landing = () => {
   return (
-    <Center>
+    <Box pt={"16%"}>
       <Flex direction="column" justify="center" w="100%">
         <Container maxSize="lg">
           <div className="title"> Hellooo </div>
@@ -18,13 +18,20 @@ export const Landing = () => {
             {" "}
             I transform data into interactive, elegant UI.{" "}
           </div>
-          <div> lil plant icon here </div>
+          <div>
+            {" "}
+            <Image
+              h={16}
+              src="https://static.tumblr.com/f18cbae0cb489e5e8ecf61a833005bbd/uvvybbn/Vdvoa2m9q/tumblr_static_5we1xrd3iswsw44w8gwggwcoc.png"
+              alt="sprout"
+            />{" "}
+          </div>
           {/* divider class */}
           <div className="divider"></div>
           <div className="location"> Credit One Bank - IT Development </div>
           <div className="links"> social media links </div>
         </Container>
       </Flex>
-    </Center>
+    </Box>
   );
 };
