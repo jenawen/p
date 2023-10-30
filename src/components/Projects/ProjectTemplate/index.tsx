e907a9ed509f48d9b591c19eac78ad9803023a05
jena adkins <jena.adkins@creditone.com>
2023-10-30 10:59:35 -0700

    projects

--- src/components/Projects/ProjectTemplate/index.tsx
+++ src/components/Projects/ProjectTemplate/index.tsx
@@ -1,37 +1,16 @@
 import { Grid, fr } from "@prismane/core";
+import "./index.css";
 
 export const ProjectTemplate = (props: any) => {
   const { title, description, picture, pills } = props;
   return (
-    <>
-      <Grid gap={fr(3)} templateColumns={8} templateRows={4} h="100%" w="100%">
-        <Grid.Item
-          columnStart={1}
-          columnEnd={3}
-          rowStart={2}
-          rowEnd={4}
-          bg="primary"
-        >
-          picture
-        </Grid.Item>
-
-        <Grid.Item columnStart={3} columnEnd={9} rowStart={2} rowEnd={2}>
-          <span className="project-title">{title}</span>
-        </Grid.Item>
-
-        <Grid.Item
-          columnStart={3}
-          columnEnd={9}
-          rowStart={3}
-          rowEnd={5}
-          h={"100%"}
-        >
-          <span className="normal-text">{description}</span>
-        </Grid.Item>
-        <Grid.Item columnStart={3} columnEnd={9} rowStart={5} rowEnd={5}>
-          pills
-        </Grid.Item>
-      </Grid>
-    </>
+    <div className="template">
+      <div>img</div>
+      <div className="text">
+        <div className="t-title">{title}</div>
+        <div className="t-desc">{description}</div>
+        <div>pills</div>
+      </div>
+    </div>
   );
 };
